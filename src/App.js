993295cadbdabc5732b21b1b90d06e2193b83d39
Alina <lindenalina.al@gmail.com>
2020-10-30 11:39:44 +0000

    radio buttons to filter by difficulty

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React, {useEffect, useState} from "react";
+import React, {Fragment, useEffect, useState} from "react";
 import axios from "axios";
 import './App.css';
 
 function App() {
 
   const [questions, setQuestions] = useState({results: []});
+  const [query, setQuery] = useState('redux');
 
-  useEffect(async () => {
-    const result = await axios(
-      'https://opentdb.com/api.php?amount=30&category=27',
-    );
-    setQuestions(result.data);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://opentdb.com/api.php?amount=30&category=27',
+      );
+      setQuestions(result.data);
+    }
+    fetchData();
   }, []);
 
   return (
-    <ul>
-      {questions.results.map((question, index) => (
-        <li key={index}>
-          <p>{question.question}</p>
-        </li>
-      ))}
-    </ul>
+    <Fragment>
+      <div>
+        <input type="radio" id="easy" name="difficulty" value="easy"></input>
+        <label for="easy">Easy</label><br></br>
+        <input type="radio" id="medium" name="difficulty" value="medium"></input>
+        <label for="medium">Medium</label><br></br>
+        <input type="radio" id="hard" name="difficulty" value="hard"></input>
+        <label for="hard">Hard</label><br></br>
+      </div>
+      <ul>
+        {questions.results.map((question, index) => (
+          <li key={index}>
+            <p>{question.question}</p>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
   )
 
 
