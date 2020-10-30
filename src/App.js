ccbf431e9a8fe3143e4100e73ca12c55f1ce63f9
Alina <lindenalina.al@gmail.com>
2020-10-30 15:20:50 +0000

    filter by difficulty

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 function App() {
 
   const [questions, setQuestions] = useState({results: []});
-  const [query, setQuery] = useState('redux');
+  const [query, setQuery] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -17,27 +18,45 @@ function App() {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    const filteredData = questions.results.filter(question => 
+        question.difficulty === query
+    );
+    setSearchResult(filteredData);
+  }, [query]);
+
+  const renderAllQuestions = () => {
+    return questions.results.map((question, index) => (
+      <li key={index}>
+        <p>{question.question}</p>
+      </li>
+    ))
+  }
+
+  const renderFilteredQuestions = () => {
+    return searchResult.map((question, index) => (
+      <li key={index}>
+        <p>{question.question}</p>
+      </li>
+    ))
+  }
   return (
     <Fragment>
       <div>
-        <input type="radio" id="easy" name="difficulty" value="easy"></input>
-        <label for="easy">Easy</label><br></br>
-        <input type="radio" id="medium" name="difficulty" value="medium"></input>
-        <label for="medium">Medium</label><br></br>
-        <input type="radio" id="hard" name="difficulty" value="hard"></input>
-        <label for="hard">Hard</label><br></br>
+        <form value={query} onChange={event => setQuery(event.target.value)}>
+          <input type="radio" id="easy" name="difficulty" value="easy"></input>
+          <label htmlFor="easy">Easy</label><br></br>
+          <input type="radio" id="medium" name="difficulty" value="medium"></input>
+          <label htmlFor="medium">Medium</label><br></br>
+          <input type="radio" id="hard" name="difficulty" value="hard"></input>
+          <label htmlFor="hard">Hard</label><br></br>
+        </form>
       </div>
       <ul>
-        {questions.results.map((question, index) => (
-          <li key={index}>
-            <p>{question.question}</p>
-          </li>
-        ))}
+        {query === '' ? renderAllQuestions() : renderFilteredQuestions()}
       </ul>
     </Fragment>
   )
-
-
 }
 
 export default App;
