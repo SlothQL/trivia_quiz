1e9020b3a839e93a9a4106824456b1e7d52bb255
Alina <lindenalina.al@gmail.com>
2020-10-30 11:24:41 +0000

    fetchng data using react hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+
+  const [questions, setQuestions] = useState({results: []});
+
+  useEffect(async () => {
+    const result = await axios(
+      'https://opentdb.com/api.php?amount=30&category=27',
+    );
+    setQuestions(result.data);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ul>
+      {questions.results.map((question, index) => (
+        <li key={index}>
+          <p>{question.question}</p>
+        </li>
+      ))}
+    </ul>
+  )
+
+
 }
 
 export default App;
